4ccc7f21f6db5a1c17a1b7422a29936f1cb72058
gassc <christian.gass@civicmapper.com>
2026-02-13 17:59:54 -0500

    fix asset paths

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/fishfrymap/" : "/",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url))
